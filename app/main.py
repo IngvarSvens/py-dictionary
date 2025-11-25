fb0ef982b8b4f65f3490bc3748cffd1fb367819f
Ihor Moskalenko <ingvarsvensson7@gmail.com>
2025-11-25 02:13:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Any, Hashable
+from typing import Any, Hashable, List, Optional
 
 
 class Node:
@@ -7,83 +7,97 @@ class Node:
         self.hash_key = hash_key
         self.value = value
 
-    def __eq__(self, other: object) -> bool:
-        return self.key == other.key and self.hash_key == other.hash_key
-
 
 class Dictionary:
     def __init__(self) -> None:
-        self.hash_table = [None for _ in range(8)]
+        self.hash_table: List[Optional[Node]] = [None] * 8
         self.current_size = 0
-        self.capacity = len(self.hash_table)
+        self.capacity = 8
 
-    def __setitem__(self, key: Hashable, value: Any) -> None:
-
-        threshold = int(0.66 * self.capacity)
-        node = Node(key, hash(key), value)
-
-        if self.current_size + 1 > threshold:
-            self.__resize_hash_table()
-
+    def _probe(self, key: Hashable, for_insert: bool = False) -> int:
         index = hash(key) % self.capacity
+        start_index = index
+
         while True:
-            if not self.hash_table[index]:
-                self.hash_table[index] = node
-                self.current_size += 1
-                break
-            elif self.hash_table[index] == node:
-                self.hash_table[index] = node
-                break
+            node = self.hash_table[index]
+
+            if node is None:
+                return index if for_insert else -1
+
+            if node.key == key and node.hash_key == hash(key):
+                return index
+
             index = (index + 1) % self.capacity
 
+            if index == start_index:
+                return index if for_insert else -1
+
     def __resize_hash_table(self) -> None:
-        old_hash_table = [node for node in self.hash_table if node is not None]
-        self.hash_table = [None for _ in range(self.capacity * 2)]
-        self.capacity = len(self.hash_table)
+        old_nodes = [node for node in self.hash_table if node is not None]
+        self.capacity *= 2
+        self.hash_table = [None] * self.capacity
         self.current_size = 0
-        self.update(old_hash_table)
+
+        for node in old_nodes:
+            self[node.key] = node.value
+
+    def __setitem__(self, key: Hashable, value: Any) -> None:
+        if self.current_size + 1 > int(self.capacity * 0.66):
+            self.__resize_hash_table()
+
+        index = self._probe(key, for_insert=True)
+        current = self.hash_table[index]
+
+        if current is None:
+            self.hash_table[index] = Node(key, hash(key), value)
+            self.current_size += 1
+        else:
+            self.hash_table[index].value = value
 
     def __getitem__(self, key: Hashable) -> Any:
-        for el in self.hash_table:
-            if el and el.key == key and el.hash_key == hash(key):
-                return el.value
-        raise KeyError(key)
+        index = self._probe(key)
+        if index == -1 or self.hash_table[index] is None:
+            raise KeyError(key)
+        return self.hash_table[index].value
 
     def __len__(self) -> int:
         return self.current_size
 
     def clear(self) -> None:
-        self.hash_table = [None for _ in range(len(self.hash_table))]
+        self.hash_table = [None] * self.capacity
+        self.current_size = 0
 
     def __delitem__(self, key: Hashable) -> None:
-        for i, el in enumerate(self.hash_table):
-            if el and el.key == key:
-                self.hash_table[i] = None
-        raise KeyError(key)
+        index = self._probe(key)
+        if index == -1 or self.hash_table[index] is None:
+            raise KeyError(key)
 
-    def get(self, key: Hashable, default_value: Any = "some value") -> Any:
+        self.hash_table[index] = None
+        self.current_size -= 1
+
+    def get(self, key: Hashable, default_value: Any = None) -> Any:
         try:
-            self.__getitem__(key)
+            return self[key]
         except KeyError:
             return default_value
 
     def pop(self, key: Hashable) -> Any:
-        value = self.__getitem__(key)
-        self.__delitem__(key)
-        return Node(key, hash(key), value)
+        value = self[key]
+        del self[key]
+        return value
 
-    def update(self, other: list[Node]) -> None:
+    def update(self, other: List[Node]) -> None:
         for node in other:
-            self.__setitem__(node.key, node.value)
+            self[node.key] = node.value
 
     def __iter__(self) -> object:
-        self.current_element = 0
+        self._iter_index = 0
         return self
 
-    def __next__(self) -> Any:
-        if self.current_element >= self.current_size:
-            raise StopIteration
-
-        result = self.hash_table[self.current_element]
-        self.current_element += 1
-        return result
+    def __next__(self) -> Hashable:
+        while self._iter_index < self.capacity:
+            node = self.hash_table[self._iter_index]
+            self._iter_index += 1
+            if node is not None:
+                return node.key
+        raise StopIteration
